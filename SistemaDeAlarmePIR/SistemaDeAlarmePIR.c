2291870bd1ef6defaf8cfd472917cc1da5ecba4a
Kaillane Martins <kaillanemartins18@gmail.com>
2025-02-06 10:55:21 -0300

    Iniciando configurações

---
+++ SistemaDeAlarmePIR/SistemaDeAlarmePIR.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+#define PIR 18
+#define RED 13
+
+int main()
+{
+    stdio_init_all();
+    gpio_init(PIR);
+    gpio_set_dir(PIR, GPIO_IN);
+    
+    gpio_init(RED);
+    gpio_set_dir(RED, GPIO_OUT);
+    gpio_put(RED, 0);
+
+    while (true) {
+        if(gpio_get(PIR) == 1){
+            gpio_put(RED, 1);
+        }else{
+            gpio_put(RED, 0);
+        }
+        sleep_ms(1000);
+    }
+}
